0cf59d6fa45809d11c31aff4f92d1d59fdefa95f
efegorkemumit <47424323+efegorkemumit@users.noreply.github.com>
2023-08-30 18:39:22 +0300

    youtube efegorkemumit

--- index.php
+++ index.php
@@ -446,9 +446,9 @@ if($result->rowCount()>0){
 		</div>
 	</div>
 
-	<!-- CTA -->
+<!-- CTA -->
 
-	<div class="cta">
+<div class="cta">
 		<!-- Image by https://unsplash.com/@thanni -->
 		<div class="cta_background" style="background-image:url(assets/images/cta.jpg)"></div>
 		
@@ -460,48 +460,35 @@ if($result->rowCount()>0){
 
 					<div class="cta_slider_container">
 						<div class="owl-carousel owl-theme cta_slider">
+							<?php 
+							try{
+								$query2= "SELECT * FROM cta_content";
+								$result2 = $conn->query($query2);
+								while($row = $result2->fetch(PDO::FETCH_ASSOC)){
+							?>
 
 							<!-- CTA Slider Item -->
 							<div class="owl-item cta_item text-center">
-								<div class="cta_title">maldives deluxe package</div>
-								<div class="rating_r rating_r_4">
+								<div class="cta_title"><?php echo $row['title']; ?></div>
+								<div class="rating_r rating_r_<?php echo $row['scor']; ?>">
 									<i></i>
 									<i></i>
 									<i></i>
 									<i></i>
 									<i></i>
 								</div>
-								<p class="cta_text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eu convallis tortor. Suspendisse potenti. In faucibus massa arcu, vitae cursus mi hendrerit nec. Proin bibendum, augue faucibus tincidunt ultrices, tortor augue gravida lectus, et efficitur enim justo vel ligula.</p>
-								<div class="button cta_button"><div class="button_bcg"></div><a href="#">book now<span></span><span></span><span></span></a></div>
+								<p class="cta_text"><?php echo $row['cta_text']; ?></p>
+								<div class="button cta_button"><div class="button_bcg"></div><a href="<?php echo $row['cta_button_link']; ?>"><?php echo $row['cta_button_text']; ?><span></span><span></span><span></span></a></div>
 							</div>
 
-							<!-- CTA Slider Item -->
-							<div class="owl-item cta_item text-center">
-								<div class="cta_title">maldives deluxe package</div>
-								<div class="rating_r rating_r_4">
-									<i></i>
-									<i></i>
-									<i></i>
-									<i></i>
-									<i></i>
-								</div>
-								<p class="cta_text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eu convallis tortor. Suspendisse potenti. In faucibus massa arcu, vitae cursus mi hendrerit nec. Proin bibendum, augue faucibus tincidunt ultrices, tortor augue gravida lectus, et efficitur enim justo vel ligula.</p>
-								<div class="button cta_button"><div class="button_bcg"></div><a href="#">book now<span></span><span></span><span></span></a></div>
-							</div>
+							<?php 
+							} }
+							catch(PDOException $e){
+								echo "Error :" .$e->getMessage();
+							}
+							?>
 
-							<!-- CTA Slider Item -->
-							<div class="owl-item cta_item text-center">
-								<div class="cta_title">maldives deluxe package</div>
-								<div class="rating_r rating_r_4">
-									<i></i>
-									<i></i>
-									<i></i>
-									<i></i>
-									<i></i>
-								</div>
-								<p class="cta_text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eu convallis tortor. Suspendisse potenti. In faucibus massa arcu, vitae cursus mi hendrerit nec. Proin bibendum, augue faucibus tincidunt ultrices, tortor augue gravida lectus, et efficitur enim justo vel ligula.</p>
-								<div class="button cta_button"><div class="button_bcg"></div><a href="#">book now<span></span><span></span><span></span></a></div>
-							</div>
+							
 							
 						</div>
 
@@ -550,6 +537,7 @@ if($result->rowCount()>0){
 	</div>
 
 	<!-- Offers -->
+
 
 	<div class="offers">
 		<div class="container">
@@ -712,178 +700,7 @@ if($result->rowCount()>0){
 		</div>
 	</div>
 
-	<!-- Testimonials -->
-
-	<div class="testimonials">
-		<div class="test_border"></div>
-		<div class="container">
-			<div class="row">
-				<div class="col text-center">
-					<h2 class="section_title">what our clients say about us</h2>
-				</div>
-			</div>
-			<div class="row">
-				<div class="col">
-					
-					<!-- Testimonials Slider -->
-
-					<div class="test_slider_container">
-						<div class="owl-carousel owl-theme test_slider">
-
-							<!-- Testimonial Item -->
-							<div class="owl-item">
-								<div class="test_item">
-									<div class="test_image"><img src="assets/images/test_1.jpg" alt="https://unsplash.com/@anniegray"></div>
-									<div class="test_icon"><img src="assets/images/backpack.png" alt=""></div>
-									<div class="test_content_container">
-										<div class="test_content">
-											<div class="test_item_info">
-												<div class="test_name">carla smith</div>
-												<div class="test_date">May 24, 2017</div>
-											</div>
-											<div class="test_quote_title">" Best holliday ever "</div>
-											<p class="test_quote_text">Nullam eu convallis tortor. Suspendisse potenti. In faucibus massa arcu, vitae cursus mi hendrerit nec.</p>
-										</div>
-									</div>
-								</div>
-							</div>
-
-							<!-- Testimonial Item -->
-							<div class="owl-item">
-								<div class="test_item">
-									<div class="test_image"><img src="assets/images/test_2.jpg" alt="https://unsplash.com/@tschax"></div>
-									<div class="test_icon"><img src="assets/images/island_t.png" alt=""></div>
-									<div class="test_content_container">
-										<div class="test_content">
-											<div class="test_item_info">
-												<div class="test_name">carla smith</div>
-												<div class="test_date">May 24, 2017</div>
-											</div>
-											<div class="test_quote_title">" Best holliday ever "</div>
-											<p class="test_quote_text">Nullam eu convallis tortor. Suspendisse potenti. In faucibus massa arcu, vitae cursus mi hendrerit nec.</p>
-										</div>
-									</div>
-								</div>
-							</div>
-
-							<!-- Testimonial Item -->
-							<div class="owl-item">
-								<div class="test_item">
-									<div class="test_image"><img src="assets/images/test_3.jpg" alt="https://unsplash.com/@seefromthesky"></div>
-									<div class="test_icon"><img src="assets/images/kayak.png" alt=""></div>
-									<div class="test_content_container">
-										<div class="test_content">
-											<div class="test_item_info">
-												<div class="test_name">carla smith</div>
-												<div class="test_date">May 24, 2017</div>
-											</div>
-											<div class="test_quote_title">" Best holliday ever "</div>
-											<p class="test_quote_text">Nullam eu convallis tortor. Suspendisse potenti. In faucibus massa arcu, vitae cursus mi hendrerit nec.</p>
-										</div>
-									</div>
-								</div>
-							</div>
-
-							<!-- Testimonial Item -->
-							<div class="owl-item">
-								<div class="test_item">
-									<div class="test_image"><img src="assets/images/test_2.jpg" alt=""></div>
-									<div class="test_icon"><img src="assets/images/island_t.png" alt=""></div>
-									<div class="test_content_container">
-										<div class="test_content">
-											<div class="test_item_info">
-												<div class="test_name">carla smith</div>
-												<div class="test_date">May 24, 2017</div>
-											</div>
-											<div class="test_quote_title">" Best holliday ever "</div>
-											<p class="test_quote_text">Nullam eu convallis tortor. Suspendisse potenti. In faucibus massa arcu, vitae cursus mi hendrerit nec.</p>
-										</div>
-									</div>
-								</div>
-							</div>
-
-							<!-- Testimonial Item -->
-							<div class="owl-item">
-								<div class="test_item">
-									<div class="test_image"><img src="assets/images/test_1.jpg" alt=""></div>
-									<div class="test_icon"><img src="assets/images/backpack.png" alt=""></div>
-									<div class="test_content_container">
-										<div class="test_content">
-											<div class="test_item_info">
-												<div class="test_name">carla smith</div>
-												<div class="test_date">May 24, 2017</div>
-											</div>
-											<div class="test_quote_title">" Best holliday ever "</div>
-											<p class="test_quote_text">Nullam eu convallis tortor. Suspendisse potenti. In faucibus massa arcu, vitae cursus mi hendrerit nec.</p>
-										</div>
-									</div>
-								</div>
-							</div>
-
-							<!-- Testimonial Item -->
-							<div class="owl-item">
-								<div class="test_item">
-									<div class="test_image"><img src="assets/images/test_3.jpg" alt=""></div>
-									<div class="test_icon"><img src="assets/images/kayak.png" alt=""></div>
-									<div class="test_content_container">
-										<div class="test_content">
-											<div class="test_item_info">
-												<div class="test_name">carla smith</div>
-												<div class="test_date">May 24, 2017</div>
-											</div>
-											<div class="test_quote_title">" Best holliday ever "</div>
-											<p class="test_quote_text">Nullam eu convallis tortor. Suspendisse potenti. In faucibus massa arcu, vitae cursus mi hendrerit nec.</p>
-										</div>
-									</div>
-								</div>
-							</div>
-
-						</div>
-
-						<!-- Testimonials Slider Nav - Prev -->
-						<div class="test_slider_nav test_slider_prev">
-							<svg version="1.1" id="Layer_6" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-								width="28px" height="33px" viewBox="0 0 28 33" enable-background="new 0 0 28 33" xml:space="preserve">
-								<defs>
-									<linearGradient id='test_grad_prev'>
-										<stop offset='0%' stop-color='#fa9e1b'/>
-										<stop offset='100%' stop-color='#8d4fff'/>
-									</linearGradient>
-								</defs>
-								<path class="nav_path" fill="#F3F6F9" d="M19,0H9C4.029,0,0,4.029,0,9v15c0,4.971,4.029,9,9,9h10c4.97,0,9-4.029,9-9V9C28,4.029,23.97,0,19,0z
-								M26,23.091C26,27.459,22.545,31,18.285,31H9.714C5.454,31,2,27.459,2,23.091V9.909C2,5.541,5.454,2,9.714,2h8.571
-								C22.545,2,26,5.541,26,9.909V23.091z"/>
-								<polygon class="nav_arrow" fill="#F3F6F9" points="15.044,22.222 16.377,20.888 12.374,16.885 16.377,12.882 15.044,11.55 9.708,16.885 11.04,18.219 
-								11.042,18.219 "/>
-							</svg>
-						</div>
-						
-						<!-- Testimonials Slider Nav - Next -->
-						<div class="test_slider_nav test_slider_next">
-							<svg version="1.1" id="Layer_7" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-							width="28px" height="33px" viewBox="0 0 28 33" enable-background="new 0 0 28 33" xml:space="preserve">
-								<defs>
-									<linearGradient id='test_grad_next'>
-										<stop offset='0%' stop-color='#fa9e1b'/>
-										<stop offset='100%' stop-color='#8d4fff'/>
-									</linearGradient>
-								</defs>
-							<path class="nav_path" fill="#F3F6F9" d="M19,0H9C4.029,0,0,4.029,0,9v15c0,4.971,4.029,9,9,9h10c4.97,0,9-4.029,9-9V9C28,4.029,23.97,0,19,0z
-							M26,23.091C26,27.459,22.545,31,18.285,31H9.714C5.454,31,2,27.459,2,23.091V9.909C2,5.541,5.454,2,9.714,2h8.571
-							C22.545,2,26,5.541,26,9.909V23.091z"/>
-							<polygon class="nav_arrow" fill="#F3F6F9" points="13.044,11.551 11.71,12.885 15.714,16.888 11.71,20.891 13.044,22.224 18.379,16.888 17.048,15.554 
-							17.046,15.554 "/>
-							</svg>
-						</div>
-
-					</div>
-					
-				</div>
-			</div>
-
-		</div>
-	</div>
-
+	
 	<div class="trending">
 		<div class="container">
 			<div class="row">
