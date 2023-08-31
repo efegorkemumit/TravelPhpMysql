418c0ee97a83bf853da18ebf05a7b6ce8f384432
efegorkemumit <47424323+efegorkemumit@users.noreply.github.com>
2023-08-31 17:26:22 +0300

    youtube efegorkemumit

--- detail/trips.php
+++ detail/trips.php
@@ -45,35 +45,73 @@ if($trip){
 
 					<div class="offers_grid">
 
-                    <div class="blog_post">
-							<div class="blog_post_image">
-								<img src="assets/images/blog_1.jpg" alt="https://unsplash.com/@anniespratt">
-								<div class="blog_post_date d-flex flex-column align-items-center justify-content-center">
-									<div class="blog_post_day">01</div>
-									<div class="blog_post_month">Dec, 2017</div>
-								</div>
-							</div>
-							<div class="blog_post_meta">
-								<ul>
-									<li class="blog_post_meta_item"><a href="">by Lore Papp</a></li>
-									<li class="blog_post_meta_item"><a href="">Uncategorized</a></li>
-									<li class="blog_post_meta_item"><a href="">3 Comments</a></li>
-								</ul>
-							</div>
-							<div class="blog_post_title"><a href="#">	<p><?php echo $row['title']; ?></p></a></div>
-							<div class="blog_post_text">
-								<p>Aenean in lacus ligula. Phasellus euismod gravida eros. Aenean nec ipsum aliquet, pharetra magna id, interdum sapien. Etiam id lorem eu nisl pellentesque semper. Nullam tincidunt metus placerat, suscipit leo ut, tempus nulla. Fusce at eleifend tellus. Ut eleifend dui nunc, non fermentum qua.</p>
-							</div>
-							<div class="blog_post_link"><a href="#">read more</a></div>
-						</div>
+                    <div class="container">
+			<div class="row">
+				<div class="col-lg-7">
+					<div class="intro_image "><img  class="m-height-600" src="<?php echo DBIMAGE; ?>/<?php echo $trip['image']; ?>" alt=""></div>
+				</div>
+				<div class="col-lg-5">
+					<div class="intro_content">
+						<div class="intro_titlec">  <?php echo $trip['title']; ?></div>
+						<p class="intro_text">  <?php echo $trip['description']; ?></p>
+						<div class="button intro_button"><div class="button_bcg"></div><a href="#">Book now<span></span><span></span><span></span></a></div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+        
 
 
 
 
-
-					
+                    
 
 					</div>
+
+                    <div class="milestones">
+		<div class="container">
+			<div class="row">
+				
+				<!-- Milestone -->
+				<div class="col-lg-3 milestone_col">
+					<div class="milestone text-center">
+						<div class="milestone_icon"><img src="<?php echo ASSETSIMAGE; ?>/milestone_1.png" alt=""></div>
+						<div class="milestone_counter" data-end-value=" <?php echo $trip['adults']; ?>">0</div>
+						<div class="milestone_text">Adults</div>
+					</div>
+				</div>
+
+				<!-- Milestone -->
+				<div class="col-lg-3 milestone_col">
+					<div class="milestone text-center">
+						<div class="milestone_icon"><img src="<?php echo ASSETSIMAGE; ?>/milestone_2.png" alt=""></div>
+						<div class="milestone_counter" data-end-value=" <?php echo $trip['children']; ?>">0</div>
+						<div class="milestone_text">Children</div>
+					</div>
+				</div>
+
+				<!-- Milestone -->
+				<div class="col-lg-3 milestone_col">
+					<div class="milestone text-center">
+						<div class="milestone_icon"><img src="<?php echo ASSETSIMAGE; ?>/milestone_3.png" alt=""></div>
+						<div class="milestone_counter" data-end-value=" <?php echo $trip['score']; ?>">0</div>
+						<div class="milestone_text">Score</div>
+					</div>
+				</div>
+
+				<!-- Milestone -->
+				<div class="col-lg-3 milestone_col">
+					<div class="milestone text-center">
+						<div class="milestone_icon"><img src="<?php echo ASSETSIMAGE; ?>/milestone_4.png" alt=""></div>
+						<div class="milestone_counter" data-end-value=" <?php echo $trip['user_rating']; ?>">0</div>
+						<div class="milestone_text">User Rating</div>
+					</div>
+				</div>
+
+			</div>
+		</div>
+	</div>
 				</div>
 
 			</div>
@@ -91,4 +129,9 @@ else {
 <script src="<?php echo APPURL;?>/assets/plugins/Isotope/isotope.pkgd.min.js"></script>
 <script src="<?php echo APPURL;?>/assets/plugins/parallax-js-master/parallax.min.js"></script>
 <script src="<?php echo APPURL;?>/assets/js/offers_custom.js"></script>
+<script src="<?php echo APPURL;?>/assets/plugins/parallax-js-master/parallax.min.js"></script>
+<script src="<?php echo APPURL;?>/assets/js/about_custom.js"></script>
+<script src="<?php echo APPURL;?>/assets/plugins/greensock/TweenMax.min.js"></script>
+<script src="<?php echo APPURL;?>/assets/plugins/greensock/TimelineMax.min.js"></script>
+<script src="<?php echo APPURL;?>/assets/plugins/scrollmagic/ScrollMagic.min.js"></script>
 
