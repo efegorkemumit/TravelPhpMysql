c754349486e662d00d3248bebe92f3fc6ba28d67
efegorkemumit <47424323+efegorkemumit@users.noreply.github.com>
2023-08-31 15:10:19 +0300

    youtube efegorkemumit

--- index.php
+++ index.php
@@ -515,78 +515,50 @@ if($result->rowCount()>0){
 		<div class="container">
 			<div class="row">
 				<div class="col text-center">
-					<h2 class="section_title">the best offers with rooms</h2>
+					<h2 class="section_title">the best offers with cruise</h2>
 				</div>
 			</div>
 			<div class="row offers_items">
 
 				<!-- Offers Item -->
+
+				<?php 
+							try{
+								$query5= "SELECT * FROM cruise ORDER BY score DESC LIMIT 4;";
+								$result5 = $conn->query($query5);
+								while($row = $result5->fetch(PDO::FETCH_ASSOC)){
+							?>
+
+
+				
 				<div class="col-lg-6 offers_col">
 					<div class="offers_item">
 						<div class="row">
+						
+							
+							
 							<div class="col-lg-6">
 								<div class="offers_image_container">
 									<!-- Image by https://unsplash.com/@kensuarez -->
-									<div class="offers_image_background" style="background-image:url(assets/images/offer_1.jpg)"></div>
-									<div class="offer_name"><a href="#">grand castle</a></div>
+									<div class="offers_image_background" style="background-image:url(<?php echo DBIMAGE; ?>/<?php echo $row['image']; ?>)"></div>
+									<div class="offer_name"><a href="#"><?php echo $row['title']; ?></a></div>
 								</div>
 							</div>
 							<div class="col-lg-6">
 								<div class="offers_content">
-									<div class="offers_price">$70<span>per night</span></div>
-									<div class="rating_r rating_r_4 offers_rating">
+									<div class="offers_price">$<?php echo $row['price']; ?><span>per night</span></div>
+									<div class="rating_r rating_r_<?php echo $row['score']; ?> offers_rating">
 										<i></i>
 										<i></i>
 										<i></i>
 										<i></i>
 										<i></i>
 									</div>
-									<p class="offers_text">Suspendisse potenti. In faucibus massa. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eu convallis tortor.</p>
-									<div class="offers_icons">
-										<ul class="offers_icons_list">
-											<li class="offers_icons_item"><img src="assets/images/post.png" alt=""></li>
-											<li class="offers_icons_item"><img src="assets/images/compass.png" alt=""></li>
-											<li class="offers_icons_item"><img src="assets/images/bicycle.png" alt=""></li>
-											<li class="offers_icons_item"><img src="assets/images/sailboat.png" alt=""></li>
-										</ul>
-									</div>
-									<div class="offers_link"><a href="offers.html">read more</a></div>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-				
-				<!-- Offers Item -->
-				<div class="col-lg-6 offers_col">
-					<div class="offers_item">
-						<div class="row">
-							<div class="col-lg-6">
-								<div class="offers_image_container">
-									<!-- Image by Egzon Bytyqi -->
-									<div class="offers_image_background" style="background-image:url(assets/images/offer_2.jpg)"></div>
-									<div class="offer_name"><a href="#">turkey hills</a></div>
-								</div>
-							</div>
-							<div class="col-lg-6">
-								<div class="offers_content">
-									<div class="offers_price">$50<span>per night</span></div>
-									<div class="rating_r rating_r_4 offers_rating">
-										<i></i>
-										<i></i>
-										<i></i>
-										<i></i>
-										<i></i>
-									</div>
-									<p class="offers_text">Suspendisse potenti. In faucibus massa. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eu convallis tortor.</p>
-									<div class="offers_icons">
-										<ul class="offers_icons_list">
-											<li class="offers_icons_item"><img src="assets/images/post.png" alt=""></li>
-											<li class="offers_icons_item"><img src="assets/images/compass.png" alt=""></li>
-											<li class="offers_icons_item"><img src="assets/images/bicycle.png" alt=""></li>
-											<li class="offers_icons_item"><img src="assets/images/sailboat.png" alt=""></li>
-										</ul>
-									</div>
+									<p class="offers_text"><?php 
+    $trim = strlen($row['description']) > 100 ? substr($row['description'], 0, 100) . '...' : $row['description'];
+    echo $trim;
+?> </p>
+									
 									<div class="offers_link"><a href="offers.html">read more</a></div>
 								</div>
 							</div>
@@ -594,79 +566,16 @@ if($result->rowCount()>0){
 					</div>
 				</div>
 
-				<!-- Offers Item -->
-				<div class="col-lg-6 offers_col">
-					<div class="offers_item">
-						<div class="row">
-							<div class="col-lg-6">
-								<div class="offers_image_container">
-									<!-- Image by https://unsplash.com/@nevenkrcmarek -->
-									<div class="offers_image_background" style="background-image:url(assets/images/offer_3.jpg)"></div>
-									<div class="offer_name"><a href="#">island dream</a></div>
-								</div>
-							</div>
-							<div class="col-lg-6">
-								<div class="offers_content">
-									<div class="offers_price">$90<span>per night</span></div>
-									<div class="rating_r rating_r_4 offers_rating">
-										<i></i>
-										<i></i>
-										<i></i>
-										<i></i>
-										<i></i>
-									</div>
-									<p class="offers_text">Suspendisse potenti. In faucibus massa. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eu convallis tortor.</p>
-									<div class="offers_icons">
-										<ul class="offers_icons_list">
-											<li class="offers_icons_item"><img src="assets/images/post.png" alt=""></li>
-											<li class="offers_icons_item"><img src="assets/images/compass.png" alt=""></li>
-											<li class="offers_icons_item"><img src="assets/images/bicycle.png" alt=""></li>
-											<li class="offers_icons_item"><img src="assets/images/sailboat.png" alt=""></li>
-										</ul>
-									</div>
-									<div class="offers_link"><a href="offers.html">read more</a></div>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
 
-				<!-- Offers Item -->
-				<div class="col-lg-6 offers_col">
-					<div class="offers_item">
-						<div class="row">
-							<div class="col-lg-6">
-								<div class="offers_image_container">
-									<!-- Image by https://unsplash.com/@mantashesthaven -->
-									<div class="offers_image_background" style="background-image:url(assets/images/offer_4.jpg)"></div>
-									<div class="offer_name"><a href="#">travel light</a></div>
-								</div>
-							</div>
-							<div class="col-lg-6">
-								<div class="offers_content">
-									<div class="offers_price">$30<span>per night</span></div>
-									<div class="rating_r rating_r_4 offers_rating">
-										<i></i>
-										<i></i>
-										<i></i>
-										<i></i>
-										<i></i>
-									</div>
-									<p class="offers_text">Suspendisse potenti. In faucibus massa. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eu convallis tortor.</p>
-									<div class="offers_icons">
-										<ul class="offers_icons_list">
-											<li class="offers_icons_item"><img src="assets/images/post.png" alt=""></li>
-											<li class="offers_icons_item"><img src="assets/images/compass.png" alt=""></li>
-											<li class="offers_icons_item"><img src="assets/images/bicycle.png" alt=""></li>
-											<li class="offers_icons_item"><img src="assets/images/sailboat.png" alt=""></li>
-										</ul>
-									</div>
-									<div class="offers_link"><a href="offers.html">read more</a></div>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
+				<?php 
+							} }
+							catch(PDOException $e){
+								echo "Error :" .$e->getMessage();
+							}
+							?>
+
+							
+							
 
 			</div>
 		</div>
@@ -682,101 +591,36 @@ if($result->rowCount()>0){
 			</div>
 			<div class="row trending_container">
 
-				<!-- Trending Item -->
-				<div class="col-lg-3 col-sm-6">
-					<div class="trending_item clearfix">
-						<div class="trending_image"><img src="assets/images/trend_1.png" alt="https://unsplash.com/@fransaraco"></div>
-						<div class="trending_content">
-							<div class="trending_title"><a href="#">grand hotel</a></div>
-							<div class="trending_price">From $182</div>
-							<div class="trending_location">madrid, spain</div>
-						</div>
-					</div>
-				</div>
+
+			<?php 
+							try{
+								$query6= "SELECT * FROM hotels ORDER BY score DESC LIMIT 8;";
+								$result6 = $conn->query($query6);
+								while($row = $result6->fetch(PDO::FETCH_ASSOC)){
+							?>
 
 				<!-- Trending Item -->
 				<div class="col-lg-3 col-sm-6">
 					<div class="trending_item clearfix">
-						<div class="trending_image"><img src="assets/images/trend_2.png" alt="https://unsplash.com/@grovemade"></div>
+						<div class="trending_image"><img src="<?php echo DBIMAGE; ?>/<?php echo $row['image']; ?>" alt="https://unsplash.com/@fransaraco"></div>
 						<div class="trending_content">
-							<div class="trending_title"><a href="#">mars hotel</a></div>
-							<div class="trending_price">From $182</div>
-							<div class="trending_location">madrid, spain</div>
+							<div class="trending_title"><a href="#"><?php echo $row['title']; ?></a></div>
+							<div class="trending_price">From $<?php echo $row['price']; ?></div>
+							<div class="trending_location"><?php echo $row['destination']; ?></div>
 						</div>
 					</div>
 				</div>
 
-				<!-- Trending Item -->
-				<div class="col-lg-3 col-sm-6">
-					<div class="trending_item clearfix">
-						<div class="trending_image"><img src="assets/images/trend_3.png" alt="https://unsplash.com/@jbriscoe"></div>
-						<div class="trending_content">
-							<div class="trending_title"><a href="#">queen hotel</a></div>
-							<div class="trending_price">From $182</div>
-							<div class="trending_location">madrid, spain</div>
-						</div>
-					</div>
-				</div>
 
-				<!-- Trending Item -->
-				<div class="col-lg-3 col-sm-6">
-					<div class="trending_item clearfix">
-						<div class="trending_image"><img src="assets/images/trend_4.png" alt="https://unsplash.com/@oowgnuj"></div>
-						<div class="trending_content">
-							<div class="trending_title"><a href="#">mars hotel</a></div>
-							<div class="trending_price">From $182</div>
-							<div class="trending_location">madrid, spain</div>
-						</div>
-					</div>
-				</div>
+				<?php 
+							} }
+							catch(PDOException $e){
+								echo "Error :" .$e->getMessage();
+							}
+							?>
 
-				<!-- Trending Item -->
-				<div class="col-lg-3 col-sm-6">
-					<div class="trending_item clearfix">
-						<div class="trending_image"><img src="assets/images/trend_5.png" alt="https://unsplash.com/@mindaugas"></div>
-						<div class="trending_content">
-							<div class="trending_title"><a href="#">grand hotel</a></div>
-							<div class="trending_price">From $182</div>
-							<div class="trending_location">madrid, spain</div>
-						</div>
-					</div>
-				</div>
 
-				<!-- Trending Item -->
-				<div class="col-lg-3 col-sm-6">
-					<div class="trending_item clearfix">
-						<div class="trending_image"><img src="assets/images/trend_6.png" alt="https://unsplash.com/@itsnwa"></div>
-						<div class="trending_content">
-							<div class="trending_title"><a href="#">mars hotel</a></div>
-							<div class="trending_price">From $182</div>
-							<div class="trending_location">madrid, spain</div>
-						</div>
-					</div>
-				</div>
 
-				<!-- Trending Item -->
-				<div class="col-lg-3 col-sm-6">
-					<div class="trending_item clearfix">
-						<div class="trending_image"><img src="assets/images/trend_7.png" alt="https://unsplash.com/@rktkn"></div>
-						<div class="trending_content">
-							<div class="trending_title"><a href="#">queen hotel</a></div>
-							<div class="trending_price">From $182</div>
-							<div class="trending_location">madrid, spain</div>
-						</div>
-					</div>
-				</div>
-
-				<!-- Trending Item -->
-				<div class="col-lg-3 col-sm-6">
-					<div class="trending_item clearfix">
-						<div class="trending_image"><img src="assets/images/trend_8.png" alt="https://unsplash.com/@thoughtcatalog"></div>
-						<div class="trending_content">
-							<div class="trending_title"><a href="#">mars hotel</a></div>
-							<div class="trending_price">From $182</div>
-							<div class="trending_location">madrid, spain</div>
-						</div>
-					</div>
-				</div>
 
 			</div>
 		</div>
